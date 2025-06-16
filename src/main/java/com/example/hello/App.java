2b6c383802493a879060cd3df94ee6494b9eed7d
EC2 Default User <ec2-user@ip-172-31-6-164.ap-south-1.compute.internal>
2025-06-16 09:46:27 +0000

    Made a change in App.java

--- src/main/java/com/example/hello/App.java
+++ src/main/java/com/example/hello/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jenkins from Bitbucket!" );
+        System.out.println( "Hello Jenkins from Bitbucket with a change!" );
     }
 }
