daa21f3b00a3ab4f9fec749cb0d1ee3b32a6737a
EC2 Default User <ec2-user@ip-172-31-6-164.ap-south-1.compute.internal>
2025-06-16 13:40:26 +0000

    A new change to trigger my Jenkins Pipeline

--- src/main/java/com/example/hello/App.java
+++ src/main/java/com/example/hello/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Jenkins to test webhook triggers!" );
+        System.out.println( "Testing a change in my hello-world application!" );
     }
 }
